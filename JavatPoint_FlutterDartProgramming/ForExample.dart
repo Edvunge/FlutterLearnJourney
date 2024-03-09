f5d3ae0db690363587ca2f432214bd98dbf1ad05
edvunge <costaedy18@gmail.com>
2024-03-09 02:44:44 +0000

    newClasses_javatPoint_dartTutorial

---
+++ JavatPoint_FlutterDartProgramming/ForExample.dart
@@ -0,0 +1,7 @@
+void main() {
+  var name = ["Peter", "Rinky Ponting", "Abhishek"];
+
+  for (var prop in name) {
+    print(prop);
+  }
+}
---
+++ JavatPoint_FlutterDartProgramming/variablesFunctions.dart
@@ -0,0 +1,12 @@
+// function declaration
+num addNumbers(num a, num b) {
+  return a + b;
+}
+
+var price1 = 29.99;
+var price2 = 20.81;
+var total = addNumbers(price1, price2);
+
+var num1 = 10;
+var num2 = 45;
+var total2 = addNumbers(num1, num2);
