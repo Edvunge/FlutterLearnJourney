7f90d068ba8e62c394b121cfa3983c4dcd2f2cdd
edvunge <costaedy18@gmail.com>
2024-03-31 02:00:20 +0100

    newCourses_dartFundamentls

---
+++ AulasDoYoutube/WashingtonDeveloper/Variaveis.dart
@@ -0,0 +1,9 @@
+void main() {
+  // var nomeCompleto = "edvaldo vunge";
+  // Object nomeCompleto = "edvaldo vunge";
+  // String nomeCompleto = "edvaldo vunge";
+  dynamic nomeCompleto = "Edvaldo Vunge";
+  nomeCompleto = 1.0;
+  
+  print(nomeCompleto);
+}
