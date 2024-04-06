50e11d9077f8e64e676e4bf8d8c337a55b91af49
edvunge <costaedy18@gmail.com>
2024-04-06 23:46:02 +0100

    newCourse

---
+++ AulasDoYoutube/Rivaan/MasterDart_In_8hs/operators.dart
@@ -0,0 +1,9 @@
+void main() {
+  var num1 = 2;
+  var num2 = 22;
+
+  print(num2 + num1);
+  print(num2 - num1);
+  print(num2 * num1);
+  print(num2 / num1);
+}
---
+++ AulasDoYoutube/Rivaan/MasterDart_In_8hs/variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  // variables
+
+  // first value
+  int first = 3;
+
+  // second value
+  int second = 21;
+
+  print(first + second);
+  print(first * second);
+  print(first - second);
+}
